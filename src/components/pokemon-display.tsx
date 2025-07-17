d349df66fd76c6a324a3330f2ebee22ee46d5f6d
Sacche <gabriel.sacchettin.123@gmail.com>
2025-07-16 22:17:31 -0300

    agrege comentario de el nombre

--- src/components/pokemon-display.tsx
+++ src/components/pokemon-display.tsx
@@ -14,7 +14,7 @@ const PokemonDisplay = ({ pokemon, isLoading, gameState }: Props) => {
   const imageUrl = pokemon?.image;
   const name = pokemon?.name;
 
-  console.log(name); // Debugging line to check the
+  console.log(name); // Debugging line to check the Pokémon name
 
   return (
     <div className="card">
